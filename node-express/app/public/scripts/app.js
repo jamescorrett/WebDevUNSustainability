b0168e0487a5a1e284fa255c8e0f20534e3cdf32
kingofnorwich <52957242+kingofnorwich@users.noreply.github.com>
2025-11-25 15:51:30 +0000

    updated pages and created json
    Started work on clientside app.js, add dom event listener and attempted set up to make it easier for more pages to be added. started a very basic json file for the homepage

---
+++ node-express/app/public/scripts/app.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+const page = document.body.dataset.page; 
+
+// this shows its the homepage, use a different more global
+// variable for the goals pages as they will have the same JS
+if (page === "home") {
+    console.log('Page loads correctly')
+
+    //content for home page
+
+}
+
+
+
+
+
+    
+}) 
