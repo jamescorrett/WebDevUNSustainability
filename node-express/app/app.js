c4fa3051256289a1a3fa975d285fbacd65ee949c
kingofnorwich <52957242+kingofnorwich@users.noreply.github.com>
2025-11-26 11:56:31 +0000

    node express setup

---
+++ node-express/app/app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+const port = 3000
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    res.sendFile("index.html", (err) => {
+        if (err)
+            console.log(err)
+    })
+})
+
+app.listen(port, () => {
+    console.log('app is listening')
+})
