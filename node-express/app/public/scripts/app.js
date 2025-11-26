6789c33259c67538b0773ff12487c6169709ffe1
kingofnorwich <52957242+kingofnorwich@users.noreply.github.com>
2025-11-26 13:49:30 +0000

    continued development of home page

--- node-express/app/public/scripts/app.js
+++ node-express/app/public/scripts/app.js
@@ -22,6 +22,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
       // content specific for pages (body page-data ="pagename")
       if (page === "home") {
+        //top row
+        const videoElement = document.getElementById("video");
+        const title = document.createElement("article");
+        videoElement.appendChild(title);
+        title.textContent = responseData.home.video[0].title;
+
+        //2nd row
+        const goalsSection = document.getElementById("goals");
+
+        for (item of responseData.home.goals) {
+          const goalsImage = document.createElement("img");
+          const goalsText = document.createElement("h3");
+
+          goalsImage.src = item.image;
+          goalsText.textContent = item.title;
+
+          goalsSection.append(goalsText, goalsImage);
+        }
       }
 
       if (page === "goals") {
