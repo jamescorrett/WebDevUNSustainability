e559a759fc64fba71c38b20c44b9eaef0d3b1b70
kingofnorwich <52957242+kingofnorwich@users.noreply.github.com>
2025-11-27 05:29:21 +0000

    continued development

--- node-express/app/public/scripts/app.js
+++ node-express/app/public/scripts/app.js
@@ -26,19 +26,38 @@ document.addEventListener("DOMContentLoaded", () => {
         navUl.appendChild(navList);
       }
 
-      const page = document.body.dataset.page;
+      const page = document.body.dataset.page; // content specific for pages (body page-data ="pagename")
 
-      // content specific for pages (body page-data ="pagename")
       if (page === "home") {
         //top row
         const videoElement = document.getElementById("video");
+<<<<<<< Updated upstream
+=======
+        const video = document.createElement("video");
+        video.src = "/assets/fish.mp4";
+        video.controls = false;
+        video.autoplay = true;
+        video.muted = true;
+        video.loop = true;
+        video.playsInline = true;
+        videoElement.appendChild(video);
+>>>>>>> Stashed changes
 
         //video goes here
         const title = document.createElement("article");
         videoElement.appendChild(title);
         title.textContent = responseData.home.video[0].title;
 
+<<<<<<< Updated upstream
         //2nd row
+=======
+        const quote = document.createElement("p");
+        quote.className = "video-quote";
+        quote.textContent =
+          '"the greatest threat to our planet is the belief that someone else will save it for us" - Robert Swan';
+        videoElement.appendChild(quote); //2nd row
+
+>>>>>>> Stashed changes
         const goalsSection = document.getElementById("goals");
 
         for (item of responseData.home.goals) {
@@ -47,16 +66,39 @@ document.addEventListener("DOMContentLoaded", () => {
 
           const imageAnchor = document.createElement("a");
           const goalsImage = document.createElement("img");
-          const goalsText = document.createElement("h3");
+          const goalsText = document.createElement("h1");
+          const hoverOverlay = document.createElement("div");
+          hoverOverlay.classList.add("hoverOverlay");
 
           goalsImage.src = item.image;
           imageAnchor.href = item.url;
           goalsText.textContent = item.title;
-          imageAnchor.append(goalsText, goalsImage);
-
+          imageAnchor.append(hoverOverlay, goalsText, goalsImage);
           goalsWrapper.append(imageAnchor);
 
           goalsSection.append(goalsWrapper);
+        }
+
+        //row 3 and 4
+        const rowsSection = document.getElementById("rows");
+
+        for (item of responseData.home.rowPages) {
+          const rowsWrapper = document.createElement("div");
+          rowsWrapper.classList.add("rowsWrapper");
+
+          const imageAnchor = document.createElement("a");
+          const rowsImage = document.createElement("img");
+          const rowsText = document.createElement("h1");
+          const hoverOverlay = document.createElement("div");
+          hoverOverlay.classList.add("hoverOverlay");
+
+          rowsImage.src = item.image;
+          imageAnchor.href = item.url;
+          rowsText.textContent = item.title;
+          imageAnchor.append(hoverOverlay, rowsText, rowsImage);
+          rowsWrapper.append(imageAnchor);
+
+          rowsSection.append(rowsWrapper);
         }
       }
 
