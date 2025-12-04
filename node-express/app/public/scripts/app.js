576c1ca3c59700fb41f72579ea965ffab384d6e5
JJboss11 <Jeromebossman78@gmail.com>
2025-12-04 01:07:40 +0000

    form page and water page update

--- node-express/app/public/scripts/app.js
+++ node-express/app/public/scripts/app.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }
 
       const page = document.body.dataset.page; // content specific for pages (body page-data ="pagename")
-
+      
       if (page === "home") {
         //top row
         const videoElement = document.getElementById("video");
@@ -149,13 +149,103 @@ document.addEventListener("DOMContentLoaded", () => {
           }
         });
       }
+      // water page 
+      if (page === "water") {
+        //top row
+      const videoElement = document.getElementById("video");
+      const video = document.createElement("video");
+      video.src = "/assets/swimming.mp4";
+      video.controls = false;
+      video.autoplay = true;
+      video.muted = true;
+      video.loop = true;
+      video.playsInline = true;
+      videoElement.appendChild(video);
 
-      if (page === "goals") {
-      }
-      if (page === "team") {
-      }
+      const title = document.createElement("article");
+      videoElement.appendChild(title);
+      title.textContent = responseData.water.video[0].title;
+
+      //Appends data from json into main section textBox elements
+      const mainSection = document.getElementById("main");
+      var children = mainSection.children;
+      var x = 0;
+      var y = 0;
+      for (var i = 0; i < children.length; i++) {
+        var tableChild = children[i];
+        if (tableChild.classList.contains("textBox")) 
+        {
+          const para = document.createElement("p");
+          tableChild.appendChild(para);
+          para.textContent = responseData.water.main[0].para[x];
+          x++;
+        }
+        else if (tableChild.classList.contains("imgBox")) 
+        {
+          const img = document.createElement("img");
+          tableChild.appendChild(img);
+          img.src = responseData.water.main[0].image[y];
+          y++;
+        }
+      }}
+
+      if (page === "team") {}
+
       if (page === "form") {
+
+        const titleElement = document.getElementById("title");
+        titleElement.textContent = responseData.form[0].title;
+
+        const nameElement = document.getElementById("nameLabel");
+        nameElement.textContent = responseData.form[0].name;
+        
+        const lastnameElement = document.getElementById("lastname");
+        lastnameElement.textContent = responseData.form[0].lastname;
+
+        const emailElement = document.getElementById("emailLabel");
+        emailElement.textContent = responseData.form[0].email;
+       
+
+        const CommentsElement = document.getElementById("Comments");
+        CommentsElement.textContent = responseData.form[0].Comments;
+
+        const ConfirmMessageElement = document.getElementById("ConfirmMessage");
+        ConfirmMessageElement.textContent = responseData.form[0].ConfirmMessage;
       }
+        
+          myForm.addEventListener('submit', (e) => {
+          //e.preventDefault();
+          ConfirmMessage.textContent = `Hi ${Name.value}, your message has been
+          received, we will contact you at ${email.value}`;
+          e.preventDefault();
+          const FormData = {
+          name: Name.value,
+          lastname: lastname.value,
+          email: email.value,
+          message: Comments.value,
+          };
+
+        const paraConfirmation = document.getElementById('Confirmation')
+   
+        fetch("/form", {
+          method: 'POST',
+          headers: {"Content-Type" : "application/json" },
+          body: JSON.stringify(FormData),
+          
+        })
+      .then(res => res.json() )
+      .then((responsedata) => {
+        console.log(responsedata);
+        paraConfirmation.textContent=`Hi ${responsedata.name}, your message
+        has been received, we will contact you at ${responsedata.email}`;
+      })
+    .catch(err => {
+        console.error(err);
+    })
+   })
+
+
+     
 
       //intersectobserve test
       // Intersection Observer setup
@@ -176,4 +266,6 @@ document.addEventListener("DOMContentLoaded", () => {
       const hiddenElements = document.querySelectorAll(".hidden");
       hiddenElements.forEach((el) => observer.observe(el));
     });
+  
 });
+
