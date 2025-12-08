0cadb610b9f3dce959640f8cf275765cc58570cf
kingofnorwich <52957242+kingofnorwich@users.noreply.github.com>
2025-12-08 17:45:08 +0000

    fixed form codespace

--- node-express/app/public/scripts/joinApp.js
+++ node-express/app/public/scripts/joinApp.js
@@ -7,24 +7,56 @@ document.addEventListener("DOMContentLoaded", () => {
   fetch("data.json")
     .then((response) => response.json())
     .then((responseData) => {
+      const titleElement = document.getElementById("title");
+      titleElement.textContent = responseData.form[0].title;
 
-      //intersectobserve test
-      // Intersection Observer setup
-      const observer = new IntersectionObserver(
-        (entries) => {
-          entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-              entry.target.classList.add("show"); // add fade-in class
-            }
+      const nameElement = document.getElementById("nameLabel");
+      nameElement.textContent = responseData.form[0].name;
+
+      const lastnameElement = document.getElementById("lastname");
+      lastnameElement.textContent = responseData.form[0].lastname;
+
+      const emailElement = document.getElementById("emailLabel");
+      emailElement.textContent = responseData.form[0].email;
+
+      const CommentsElement = document.getElementById("Comments");
+      CommentsElement.textContent = responseData.form[0].Comments;
+
+      const ConfirmMessageElement = document.getElementById("ConfirmMessage");
+      ConfirmMessageElement.textContent = responseData.form[0].ConfirmMessage;
+
+      const myForm = document.getElementById("myForm");
+      const Name = document.getElementById("Name");
+      const lastname = document.getElementById("lastname");
+      const email = document.getElementById("email");
+      const Comments = document.getElementById("Comments");
+      const paraConfirmation = document.getElementById("Confirmation");
+
+      myForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        ConfirmMessageElement.textContent = `Hi ${Name.value}, your message has been received, we will contact you at ${email.value}`;
+
+        const formData = {
+          name: Name.value,
+          lastname: lastname.value,
+          email: email.value,
+          message: Comments.value,
+        };
+
+        fetch("/form", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formData),
+        })
+          .then((res) => res.json())
+          .then((responsedata) => {
+            console.log(responsedata);
+            paraConfirmation.textContent = `Hi ${responsedata.name}, your message has been received, we will contact you at ${responsedata.email}`;
+          })
+          .catch((err) => {
+            console.error(err);
           });
-        },
-        {
-          threshold: 0.2, // triggers when 20% of element is visible
-        }
-      );
-
-      // select all elements with the hidden class
-      const hiddenElements = document.querySelectorAll(".hidden");
-      hiddenElements.forEach((el) => observer.observe(el));
+      });
     });
-})
+});
